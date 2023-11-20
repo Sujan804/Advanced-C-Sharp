ca07aae8ae3c83f2c6f27ddd4f8166805a69c24b
Sujan804 <sujanmec804@gmail.com>
2023-11-20 12:51:33 +0600

    Delegates basic done

--- DelegateBasic/Program.cs
+++ DelegateBasic/Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using System.IO;
 namespace DelegateBasic
 {
-    internal class Program
+    public class Program
     {
+        delegate void LogDel(string message);
         static void Main(string[] args)
         {
+            Log log = new Log();
+            LogDel LogTextToScreenDel, LogTextToFileDel;
+            LogTextToScreenDel = new LogDel(log.LogToScreen);
+            LogTextToFileDel = new LogDel(log.LogToFile);
+            LogDel multiLog = LogTextToScreenDel + LogTextToFileDel;
+            var name = Console.ReadLine();
+            multiLog(name);
+
+        }
+    }
+    public class Log
+    {
+        public void LogToScreen(string message)
+        {
+            Console.WriteLine($"{DateTime.Now}: {message}");
+        }
+        public void LogToFile(string message)
+        {
+            using(StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log.txt"),true))
+            {
+                sw.WriteLine($"{DateTime.Now}: {message}");
+            }
         }
     }
 }
